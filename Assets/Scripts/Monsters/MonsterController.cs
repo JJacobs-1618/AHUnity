89dc9c07d3353764b455c4b920a014dcff834bc3
JJ <jej170000@utdallas.edu>
2023-05-11 13:05:25 -0500

    Added MoveType to Monsters

--- Assets/Scripts/Monsters/MonsterController.cs
+++ Assets/Scripts/Monsters/MonsterController.cs
@@ -5,8 +5,17 @@ using UnityEngine;
 public class MonsterController : MonoBehaviour
 {
     [SerializeField] DimensionalSymbol dimSym;
+    [SerializeField] MoveType moveType;
 }
 
+enum MoveType
+{
+    Normal,
+    Stationary,
+    Fast,
+    Unique,
+    Flying
+}
 public enum DimensionalSymbol 
 {
     Square,
